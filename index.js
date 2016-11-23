3ad865f1f81c03b33c007eb4326a028d07ac84df
Alyssa <alyssaw90@gmail.com>
2016-11-23 00:52:25 -0800

    finishing initial set up

--- index.js
+++ index.js
@@ -4,4 +4,11 @@ var app = express();
 
 app.set('view engine', 'ejs')
 
-app.use('ejsLayouts');
+app.use(ejsLayouts);
+app.use(express.static(__dirname + "/public"));
+
+var homeCtrl = require('./controllers/home');
+
+app.use("/", homeCtrl);
+
+app.listen(3000);
