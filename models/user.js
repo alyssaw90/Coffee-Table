16ecee267d65c012126376a31918f3f9826928d4
Alyssa <alyssaw90@gmail.com>
2016-11-23 22:27:53 -0800

    added user model

---
+++ models/user.js
@@ -0,0 +1,22 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var user = sequelize.define('user', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: DataTypes.STRING,
+    description: DataTypes.TEXT
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return user;
+};
