39d4fada16f3f91bdca87dd2382fdb4b7b317cb9
Alyssa <alyssaw90@gmail.com>
2016-11-28 22:33:12 -0800

    fixing sessions

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 'use strict';
 
+var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
-var session = require('express-session');
+var bodyParser = require('body-parser');
 var passport = require('./config/ppConfig');
+var session = require('express-session');
 var flash = require('connect-flash');
 var isLoggedIn = require('./middleware/isLoggedIn');
-var express = require('express');
 var app = express();
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
-app.use(express.static(__dirname + "/public"));
 app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true
+//   secret: process.env.SESSION_SECRET,
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
 }));
+app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(flash());
-app.use(function(req, res, next){
-    res.locals.alerts = req.flash();
-    res.locals.currentUser = req.user;
-    next();
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.alerts = req.flash();
+  next();
 });
 
 var homeCtrl = require('./controllers/home');
@@ -36,4 +39,6 @@ app.get('/profile', isLoggedIn, function(req, res){
     res.render('profile');
 })
 
-app.listen(3000);
+var server = app.listen(process.env.PORT || 3000);
+
+module.exports = server;
